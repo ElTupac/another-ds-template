3ce0632d2ca3163dea511f073da73502edb6af5e
ElTupac <62774311+ElTupac@users.noreply.github.com>
2022-07-17 21:48:31 -0300

    Added some config files for code quality

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+const path = require("path");
+
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["plugin:react/recommended", "airbnb", "prettier"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "import/no-unresolved": "off",
+    "react/jsx-props-no-spreading": "off",
+    "no-console": ["warn", { allow: ["warn", "error"] }],
+    "react/function-component-definition": "off",
+    "react/jsx-filename-extension": "off",
+    "react/react-in-jsx-scope": "off",
+    "import/no-cycle": "off",
+    "react/button-has-type": "off",
+    "react/require-default-props": "off",
+  },
+  settings: {
+    react: {
+      version: "latest",
+    },
+    "import/resolver": {
+      alias: {
+        map: [["@", path.resolve(__dirname, "src/ds")]],
+      },
+    },
+  },
+};
